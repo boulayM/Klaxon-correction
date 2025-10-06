6639402238934c7d9782012217d954dd2ddac22e
Boulay.M <macboulay2@gmail.com>
2025-10-06 19:54:50 +0200

    docker

--- Config/env.php
+++ Config/env.php
@@ -4,5 +4,5 @@ return [
     'db_host' => $_ENV['DB_HOST'] ?? 'localhost',
     'db_name' => $_ENV['DB_NAME'] ?? 'testKlaxon',
     'db_user' => $_ENV['DB_USER'] ?? 'root',
-    'db_pass' => $_ENV['DB_PASS'] ?? '',
+    'db_pass' => $_ENV['DB_PASS'] ?? 'root',
 ];
